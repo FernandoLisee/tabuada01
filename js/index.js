1318f54b2bb5a6603cf5eb9bd0f4459ecd87f97a
FernandoLisee <fernandolise23@gmail.com>
2019-08-07 17:03:31 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+for (i = 0; i < 10; i++){
+	for (j = 0; j < 10; j++){
+			console.log(i + " * " + j + " = " + i*j);
+			document.body.append(i + " * " + j + " = " + i*j);
+			document.write ("<br>");
+	}
+}
